de359568bd4c08c634b9b35c2acfd03c2e605499
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-11-03 13:29:36 +0100

    Start proper layout

---
+++ layout.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mark Howells-Mead</title>
+    <link rel="stylesheet" href="Resources/Public/Css/css-reset.css">
+    <link rel="stylesheet" href="Resources/Public/Css/style.css">
+</head>
+<body>
+<div class="container">
+
+<header class="mod page-header">
+    <h1 class="page-title">Mark Howells-Mead</h1>
+    <h2 class="page-subtitle">Web developer and photographer</h2>
+    <nav class="nav main">
+        <ul class="inline">
+            <li class="menu-item">
+                <a href="https://profiles.wordpress.org/markhowellsmead" class="is-icon wordpress">WordPress</a>
+            </li>
+            <li class="menu-item">
+                <a href="https://twitter.com/mhmli" class="is-icon twitter">Twitter</a>
+            </li>
+            <li class="menu-item">
+                <a href="https://www.facebook.com/mhowellsmead" class="is-icon facebook">Facebook</a>
+            </li>
+            <li class="menu-item">
+                <a href="https://wpch.slack.com/" class="is-icon slack">Slack</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<section class="content">
+    <main class="mod main">
+
+        <article class="post">
+            <header class="post-header">
+                <h2 class="post-title">
+                    <a rel="bookmark" href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">The view around the corner at Susten Pass</a></h2>
+                    <time datetime="2001-05-15T19:00">23<sup>rd</sup> October 2016</time>
+            </header>
+            <div class="excerpt">
+                <p>Finding a magnificent view of a scene I know well, by going up a little road and turning a corner.</p>
+            </div>
+            <footer class="post-footer">
+                <p>Posted in <a href="https://permanenttourist.ch/category/viewpoints/" rel="tag">Photographic viewpoints in Switzerland</a> ~ <a href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">Read this post »</a></p>
+            </footer>
+        </article>
+
+        <article class="post">
+            <header class="post-header">
+                <h2 class="post-title">
+                    <a rel="bookmark" href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">The view around the corner at Susten Pass</a></h2>
+                    <time datetime="2001-05-15T19:00">23<sup>rd</sup> October 2016</time>
+            </header>
+            <div class="excerpt">
+                <p>Finding a magnificent view of a scene I know well, by going up a little road and turning a corner.</p>
+            </div>
+            <footer class="post-footer">
+                <p>Posted in <a href="https://permanenttourist.ch/category/viewpoints/" rel="tag">Photographic viewpoints in Switzerland</a> ~ <a href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">Read this post »</a></p>
+            </footer>
+        </article>
+
+        <article class="post">
+            <header class="post-header">
+                <h2 class="post-title">
+                    <a rel="bookmark" href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">The view around the corner at Susten Pass</a></h2>
+                    <time datetime="2001-05-15T19:00">23<sup>rd</sup> October 2016</time>
+            </header>
+            <div class="excerpt">
+                <p>Finding a magnificent view of a scene I know well, by going up a little road and turning a corner.</p>
+            </div>
+            <footer class="post-footer">
+                <p>Posted in <a href="https://permanenttourist.ch/category/viewpoints/" rel="tag">Photographic viewpoints in Switzerland</a> ~ <a href="https://permanenttourist.ch/2016/10/the-view-around-the-corner-at-susten-pass/">Read this post »</a></p>
+            </footer>
+        </article>
+
+    </main>
+
+    <aside class="mod aside">
+        <nav class="nav aside">
+        <ul>
+            <li class="menu-item">
+                <a href="#">Menu entry 1</a>
+            </li>
+            <li class="menu-item">
+                <a href="#">Menu entry 2</a>
+            </li>
+            <li class="menu-item">
+                <a href="#">Menu entry 3</a>
+            </li>
+            <li class="menu-item">
+                <a href="#">Menu entry 4</a>
+            </li>
+        </ul>
+    </nav>
+    </aside>
+</section>
+
+<footer class="page-footer">No copyright here. Steal and re-use anything at markweb.ch for <a href="https://github.com/mhmli/markweb.ch/blob/master/LICENSE">whatever you like</a>.</footer>
+
+</div>
+
+</body>
+</html>
